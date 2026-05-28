4170f9c107dfe719e171a73ff4c36445777bca95
M. Eleonora Rossi <46747174+MEleonoraRossi@users.noreply.github.com>
2026-05-28 17:49:55 +0200

    Added scripts

---
+++ Scripts/Commands_used.sh
@@ -0,0 +1,39 @@
+#MAfft
+for f in *gaps; do mafft $f > $f.mafft; done
+#Concatenaion
+perl FASconCAT-G_v1.04.pl -l -s
+#Iqtree genetrees 
+#THis is for LG but for the others we just used -m LG+C60+G and -m Poisson+G
+JOB_ARRAY_FILE=`find . -maxdepth 1 -iname "*.mafft" | sed -n "${SLURM_ARRAY_TASK_ID}p"`
+
+iqtree -s ${JOB_ARRAY_FILE} -m LG+G -bb 1000 -T 12 
+
+#IQtree concatenation
+#LG
+iqtree -s FcC_supermatrix.fas -m LG+G -bb 1000 -T 12
+#LGC60
+iqtree -s FcC_supermatrix.fas -m LG+C60+G  -bb 1000 -T 12
+#Poisson
+iqtree -s FcC_supermatrix.fas -m Poisson+G -bb 1000 -T 12 
+
+#ASTRAL
+#to generate the treelist
+ cat *treefile > Mollusca_astral.treefile
+#run astral
+astral -i Mollusca_astral.treefile -o ASTRAL_topology_mollusca.tre
+
+#Phylobayes
+```sh
+module load openmpi/5.0.3
+module load phylobayesmpi/1.9
+ 
+cd $SLURM_SUBMIT_DIR
+  
+mpirun -np 8 --oversubscribe \
+    pb_mpi \
+    -d Mollusca_FcC_supermatrix.phy \
+    -cat \
+    -poisson \
+    -x 1 10000 \
+    Mollusca_catpoisson_chain1
+```
--- Scripts/Phylobayes.sh
+++ Scripts/Phylobayes.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-
-#SBATCH --job-name=Alisim
-#SBATCH --mem=10G
+#SBATCH --job-name=PB2
+#SBATCH --mem=100G
 #SBATCH --nodes=1
-#SBATCH --account=gely021262
-#SBATCH --tasks-per-node=1
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1
-#SBATCH --time=1-00:00:00
+#SBATCH --time=6-00:00:00
 #SBATCH --partition=Innovation
+#SBATCH --account=gely021262
 
-#directory
-job_dir=$( pwd )
-cd $job_dir
-
-source ~/initMamba.sh
-conda activate Physalia
-
-mpirun  -d FcC_supermatrix.fas -cat -gtr Mollusca_chain1
+ 
+module load openmpi/5.0.3
+module load phylobayesmpi/1.9
+ 
+cd $SLURM_SUBMIT_DIR
+  
+mpirun -np 8 --oversubscribe \
+    pb_mpi \
+    -d Mollusca_FcC_supermatrix.phy \
+    -cat \
+    -poisson \
+    -x 1 10000 \
+    Mollusca_catpoisson_chain2
